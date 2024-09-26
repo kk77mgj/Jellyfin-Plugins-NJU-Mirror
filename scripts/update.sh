face716156e0254348090264fdfb9ae3a8ad6df8
kk77mgj <zytumgj@gmail.com>
2024-09-26 14:50:38 +0800

    1.修改换行符至unix风格 2.提前创建空目录

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # download manifest.json
+if ../temp not exists; then
+    mkdir ../temp
+fi
 wget -p ../temp https://mirrors.nju.edu.cn/jellyfin/files/plugin/manifest.json
 
 # update url
 if ../temp/manifest.json exists; then
     sed -i 's_https://repo.jellyfin.org_https://mirrors.nju.edu.cn/jellyfin_' ../temp/manifest.json
+    if ../manifests not exists; then
+        mkdir ../manifests
+    fi
     mv ../temp/manifest.json ../manifests/manifest.json
     git config --global user.email "github-actions[bot]@users.noreply.github.com"
     git config --global user.name "GitHub Actions [Bot]"
