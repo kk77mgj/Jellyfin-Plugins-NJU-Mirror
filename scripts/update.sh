38b99f0d4a385f12c75c4881702f941180e5e752
kk77mgj <zytumgj@gmail.com>
2024-09-26 15:31:20 +0800

    Fix bug

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # download manifest.json
-if ../temp not exists; then
-    mkdir ../temp
+if [ ! -d temp ]; then
+    mkdir temp
 fi
-wget -p ../temp https://mirrors.nju.edu.cn/jellyfin/files/plugin/manifest.json
+wget -p temp https://mirrors.nju.edu.cn/jellyfin/files/plugin/manifest.json
 
 # update url
-if ../temp/manifest.json exists; then
-    sed -i 's_https://repo.jellyfin.org_https://mirrors.nju.edu.cn/jellyfin_' ../temp/manifest.json
-    if ../manifests not exists; then
-        mkdir ../manifests
+if [ -f temp/manifest.json ] ; then
+    sed -i 's_https://repo.jellyfin.org_https://mirrors.nju.edu.cn/jellyfin_' temp/manifest.json
+    if [ ! -d manifest ]; then
+        mkdir manifests
     fi
-    mv ../temp/manifest.json ../manifests/manifest.json
+    mv temp/manifest.json manifests/manifest.json
     git config --global user.email "github-actions[bot]@users.noreply.github.com"
     git config --global user.name "GitHub Actions [Bot]"
-    git add ../manifests/manifest.json
+    git add manifests/manifest.json
     git commit -m "update manifest.json"
 fi
